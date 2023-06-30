128d3e3c74c48c756aa3e4b550ddda5052a89a38
Billal Fauzan <billal.xcode@gmail.com>
2023-06-30 22:21:39 +0700

    mengubah judul proyek

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Wifi Management'),
+    'name' => env('APP_NAME', 'WifiManagement'),
 
     /*
     |--------------------------------------------------------------------------
