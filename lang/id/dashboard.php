b75c1bb1fd50bfe09d7f735ca53f240c2faf2724
Billal Fauzan <billal.xcode@gmail.com>
2023-06-30 23:31:22 +0700

    multilanguage: add indonesian language

---
+++ lang/id/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'welcome-user' => 'Hai, :user',
+    'new-router' => 'Router baru',
+    'total-router' => 'Jumlah router',
+    'connected' => 'Terkoneksi',
+    'free-edition' => 'Edisi Gratis',
+    'router-list' => 'Router list'
+];
---
+++ lang/id/hotspot.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'dashboard-router' => 'Dashboard router: :name',
+    'date-time' => "Tanggal & Waktu",
+    'online' => 'Online',
+    'hotspot-active' => 'Hotspot Aktif',
+    'total-voucher' => 'Jumlah voucher',
+    'hotspot-item' => 'Hotspot Items',
+    'resource' => 'Sumber',
+    'traffic-interface' => 'Antarmuka Lalu Lintas',
+    'edit-role' => 'Ubah Role',
+    'router-name' => "Nama Router",
+    'server-location' => 'Lokasi server',
+    'choice-server-location' => 'Pilih lokasi server',
+    'save' => 'Save',
+    'test-connection' => "Tes koneksi",
+    'voucher' => [
+        'list-voucher' => 'List voucher :name',
+        'add-voucher' => 'Tambah voucher',
+        'generate-voucher' => 'Generate voucher',
+        'list-voucher-on' => 'List voucher di: :name',
+        'all-voucher' => 'Semua voucher'
+    ]
+];
