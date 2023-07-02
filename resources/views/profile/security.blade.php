0ad7cb8128d88d316b230fea59926fa77756685f
Billal Fauzan <billal.xcode@gmail.com>
2023-07-02 18:13:54 +0700

    add security page

---
+++ resources/views/profile/security.blade.php
@@ -0,0 +1,90 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="col-auto mb-3">
+            <h1 class="page-header-title">
+                <div class="page-header-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
+                    </svg></div>
+                Account Setting
+            </h1>
+        </div>
+    </x-slot>
+
+    <div class="container-xl px-4 mt-4">
+        @include('profile.partials.navigation')
+        <hr class="mt-0 mb-4">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card mb-4">
+                    <div class="card-header">Change Password</div>
+                    <div class="card-body">
+                        <form>
+                            <!-- Form Group (current password)-->
+                            <div class="mb-3">
+                                <label class="small mb-1" for="currentPassword">Current Password</label>
+                                <input class="form-control" id="currentPassword" type="password" placeholder="Enter current password">
+                            </div>
+                            <!-- Form Group (new password)-->
+                            <div class="mb-3">
+                                <label class="small mb-1" for="newPassword">New Password</label>
+                                <input class="form-control" id="newPassword" type="password" placeholder="Enter new password">
+                            </div>
+                            <!-- Form Group (confirm password)-->
+                            <div class="mb-3">
+                                <label class="small mb-1" for="confirmPassword">Confirm Password</label>
+                                <input class="form-control" id="confirmPassword" type="password" placeholder="Confirm new password">
+                            </div>
+                            <button class="btn btn-primary" type="button">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-12">
+                <div class="card mb-4 mb-xl-0">
+                    <div class="card-header">Delete Account</div>
+                    <div class="card-body">
+                        <p>
+                            Deleting your account is a permanent action and cannot be undone. If you are sure you want to delete your account, select the button below.
+                        </p>
+
+                        <x-danger-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">{{ __('Delete Account') }}</x-danger-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+            <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+                @csrf
+                @method('delete')
+
+                <h2 class="text-lg font-medium text-gray-900">
+                    {{ __('Are you sure you want to delete your account?') }}
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                </p>
+
+                <div class="mt-6">
+                    <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+
+                    <x-text-input id="password" name="password" type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}" />
+
+                    <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <x-secondary-button x-on:click="$dispatch('close')">
+                        {{ __('Cancel') }}
+                    </x-secondary-button>
+
+                    <x-danger-button class="ml-3">
+                        {{ __('Delete Account') }}
+                    </x-danger-button>
+                </div>
+            </form>
+        </x-modal>
+    </div>
+
+</x-app-layout>
